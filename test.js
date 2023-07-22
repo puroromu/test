4c9e721d3e1f04f910cff0452f819b8b9ecbaf9f
puroromu <132409407+puroromu@users.noreply.github.com>
2023-07-22 16:20:42 +0900

    Update test.js

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ client.on("ready", () => {
 
   client.on("message", (message) => {
     if (message.content === "!allban") {
-
+      // client.guilds.first().ban(user);
       // !allbanというメッセージを削除する
       message.delete();
     }
